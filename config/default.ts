8a90226b583f5814c30433d34fa764c6c5116d82
lonebots <jishnusurajila@gmail.com>
2022-11-03 18:13:52 +0530

    added initial config

---
+++ config/default.ts
@@ -0,0 +1,6 @@
+export default {
+  port: 1338,
+  hostname: "localhost",
+  dbUri:
+    "mongodb+srv://testuser:KnQdo2Np2BrNit2z@cluster0.hdp4pop.mongodb.net/rest-api-server-2?retryWrites=true&w=majority",
+};
