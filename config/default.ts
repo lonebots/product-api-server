9847adfe859634eb369c9d4338c16148d15df3eb
lonebots <jishnusurajila@gmail.com>
2022-11-04 18:55:48 +0530

    update config --added saltWorkFactor

--- config/default.ts
+++ config/default.ts
@@ -3,4 +3,6 @@ export default {
   hostname: "localhost",
   dbUri:
     "mongodb+srv://testuser:KnQdo2Np2BrNit2z@cluster0.hdp4pop.mongodb.net/rest-api-server-2?retryWrites=true&w=majority",
-};
+    saltWorkFactor : 10,
+
+  };
