b4e2a13647402af124f3eadb82bc5e9f37f39aa9
lonebots <jishnusurajila@gmail.com>
2022-11-05 12:50:38 +0530

    update controller --> user added request type

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
 import logger from "../utils/logger";
+import { CreateUserInput } from "../schema/user.schema";
 
-
-// for creating user 
-export async function createUserHandler(req: Request, res: Response) {
+// for creating user
+// we can specify what a request body should expect
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
   try {
     const user = await createUser(req.body); // call create user service
   } catch (e: any) {
