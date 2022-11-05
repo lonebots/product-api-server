56d37b1794ac49ce3d79eaf82dbd4bf7b73dd451
lonebots <jishnusurajila@gmail.com>
2022-11-05 12:52:29 +0530

    update route --> added /api/users --post --> user creation

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validateResource from './middleware/validateResource'
+import { createUserSchema } from "./schema/user.schema";
+
 function routes(app: Express) {
-  //health check route
+  // health check route
   app.get("/healthcheck", (req: Request, res: Response) => {
     res.sendStatus(200);
   });
+
+  // create user
+  app.post("/api/users",validateResource(createUserSchema) ,createUserHandler);
 }
 
 export default routes;
