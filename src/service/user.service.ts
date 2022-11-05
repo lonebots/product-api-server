121232d04ea5a9a96417fa08e51fcf57454bec64
lonebots <jishnusurajila@gmail.com>
2022-11-05 12:51:11 +0530

    added service --> user servcie --create user

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,7 +2,11 @@ import { DocumentDefinition } from "mongoose";
 import userModel, { UserDocument } from "../models/user.model";
 
 // create user service
-export async function createUser(input: DocumentDefinition<UserDocument>) {
+export async function createUser(
+  input: DocumentDefinition<
+    Omit<UserDocument, "createdAt" | "updatedAt" | "comparePassword">
+  >
+) {
   try {
     return await userModel.create(input);
   } catch (e: any) {
