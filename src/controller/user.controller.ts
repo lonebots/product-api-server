21de6f53402ad5bf929e61beddc410d278e7bc00
lonebots <jishnusurajila@gmail.com>
2022-11-05 09:43:16 +0530

    src/controller/user.controller.ts

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { createUser } from "../service/user.service";
 import logger from "../utils/logger";
 
+
+// for creating user 
 export async function createUserHandler(req: Request, res: Response) {
   try {
     const user = await createUser(req.body); // call create user service
