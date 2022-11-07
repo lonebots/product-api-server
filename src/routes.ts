292317b109f765ca159fb3c8e60276d587fa75fe
lonebots <jishnusurajila@gmail.com>
2022-11-08 01:10:46 +0530

    update routes --> added /api/session --> GET

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,12 @@
 import { Express, Request, Response } from "express";
+import {
+  createUserSessionHandler,
+  getUserSessionsHandler,
+} from "./controller/session.controller";
 import { createUserHandler } from "./controller/user.controller";
-import validateResource from './middleware/validateResource'
+import requireUser from "./middleware/requireUser";
+import validateResource from "./middleware/validateResource";
+import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
@@ -10,7 +16,16 @@ function routes(app: Express) {
   });
 
   // create user
-  app.post("/api/users",validateResource(createUserSchema) ,createUserHandler);
-}
+  app.post("/api/users", validateResource(createUserSchema), createUserHandler);
 
+  // create session
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
+
+  // get all routes
+  app.get("/api/sessions", requireUser, getUserSessionsHandler);
+}
 export default routes;
