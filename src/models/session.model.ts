d0e5b10b8e102491f203ca425337b2c65f703f50
lonebots <jishnusurajila@gmail.com>
2022-11-08 01:09:33 +0530

    \ session model added

--- src/models/session.model.ts
+++ src/models/session.model.ts
@@ -18,7 +18,7 @@ const sessionSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     valid: { type: Boolean, default: true },
-    userAgent: { type: string },
+    userAgent: { type: String },
   },
   {
     timestamps: true,
