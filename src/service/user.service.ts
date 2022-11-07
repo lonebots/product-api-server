fa16c7f3747cd397558b7e31892ca06d5555f649
lonebots <jishnusurajila@gmail.com>
2022-11-07 21:22:11 +0530

    update user service --> type added for user model

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,6 @@
 import { DocumentDefinition } from "mongoose";
 import userModel, { UserDocument } from "../models/user.model";
+import { omit } from "lodash";
 
 // create user service
 export async function createUser(
@@ -8,8 +9,28 @@ export async function createUser(
   >
 ) {
   try {
-    return await userModel.create(input);
+    const user = await userModel.create(input);
+    return omit(user.toJSON(), "password");
   } catch (e: any) {
     throw new Error(e);
   }
+}
+
+// validate password
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return false;
+  }
+  const isValid = await user.comparePassword(password);
+  if (!isValid) {
+    return false;
+  }
+  return omit(user.toJSON(), "password");
 }
