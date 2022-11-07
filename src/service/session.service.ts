c00ff5dccc59fa2e367104e4e9a7efb599f564ca
lonebots <jishnusurajila@gmail.com>
2022-11-08 01:09:54 +0530

    session service added

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,8 +1,14 @@
-import SessionModel from "../models/session.model";
+import { FilterQuery } from "mongoose";
+import SessionModel, { sessionDocument } from "../models/session.model";
 
 // create a session
 export async function createSession(userId: string, userAgent: string) {
   const session = await SessionModel.create({ user: userId, userAgent });
 
   return session.toJSON();
+}
+
+export async function findSessions(query: FilterQuery<sessionDocument>) {
+  return SessionModel.find(query).lean();
+  // lean ensures that it won't return all the object of mongoose and similar to the json
 }
