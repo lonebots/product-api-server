c8f48742969eadec9057eac167b65a7d52e9a95d
lonebots <jishnusurajila@gmail.com>
2022-11-07 21:41:32 +0530

    added session service

---
+++ src/service/session.service.ts
@@ -0,0 +1,8 @@
+import SessionModel from "../models/session.model";
+
+// create a session
+export async function createSession(userId: string, userAgent: string) {
+  const session = await SessionModel.create({ user: userId, userAgent });
+
+  return session.toJSON();
+}
