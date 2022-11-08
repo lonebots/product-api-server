67f9371ffeea5eb1b46ace8bbbd855bee6f2cb5e
lonebots <jishnusurajila@gmail.com>
2022-11-08 23:18:37 +0530

    update session service --> added reissue token function

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -1,5 +1,10 @@
 import { FilterQuery, UpdateQuery } from "mongoose";
 import SessionModel, { sessionDocument } from "../models/session.model";
+import { signJwt, verifyJwt } from "../utils/jwt.utils";
+import { get } from "lodash";
+import { findUser } from "./user.service";
+import config from "config";
+import logger from "../utils/logger";
 
 // create a session
 export async function createSession(userId: string, userAgent: string) {
@@ -14,6 +19,35 @@ export async function findSessions(query: FilterQuery<sessionDocument>) {
 }
 
 // updating session
-export async function updateSession(query: FilterQuery<sessionDocument>, update : UpdateQuery<sessionDocument>){
-  return SessionModel.updateOne(query,update)
+export async function updateSession(
+  query: FilterQuery<sessionDocument>,
+  update: UpdateQuery<sessionDocument>
+) {
+  return SessionModel.updateOne(query, update);
+}
+
+// reissue the access token
+export async function reIssueAccessToken({
+  refreshToken,
+}: {
+  refreshToken: string;
+}) {
+  const { decoded } = verifyJwt(refreshToken);
+
+  if (!decoded || !get(decoded, "session")) return false;
+
+  const session = await SessionModel.findById(get(decoded, "session"));
+
+  if (!session || !session.valid) return false;
+
+  const user = await findUser({ _id: session.user });
+
+  if (!user) return false;
+
+  const accessToken = signJwt(
+    { ...user, session: session._id },
+    { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+  );
+
+  return accessToken;
 }
