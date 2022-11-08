bd8d96e2df0be6294aeddbb85d1590cc0957a1b4
lonebots <jishnusurajila@gmail.com>
2022-11-08 23:18:00 +0530

    update deserialize user --> added token reissue logic

--- src/middleware/deserializeuser.ts
+++ src/middleware/deserializeuser.ts
@@ -1,19 +1,46 @@
 import { NextFunction, Request, Response } from "express";
 import { get } from "lodash"; // make it easy for retriving properties which we doesn't know if it exist or not
+import { string } from "zod";
+import { reIssueAccessToken } from "../service/session.service";
 import { verifyJwt } from "../utils/jwt.utils";
+import logger from "../utils/logger";
 
-const deserializeUser = (req: Request, res: Response, next: NextFunction) => {
-  const accessToken = get(req, "headers.authorization", "")?.replace(
+const deserializeUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = get(req, "headers.authorization", "").replace(
     /^Bearer\s/,
     ""
-  ); // remove the word Bearer from the front of the accesstoken
-  if (!accessToken) return next();
+  );
+
+  const refreshToken = get(req, "headers.x-refresh") as string;
+
+  if (!accessToken) {
+    return next();
+  }
 
   const { decoded, expired } = verifyJwt(accessToken);
+
   if (decoded) {
     res.locals.user = decoded;
     return next();
   }
+
+  if (expired && refreshToken) {
+    const newAccessToken = await reIssueAccessToken({ refreshToken });
+
+    if (newAccessToken) {
+      res.setHeader("x-access-token", newAccessToken);
+    }
+
+    const result = verifyJwt(newAccessToken as string);
+
+    res.locals.user = result.decoded;
+    return next();
+  }
+
   return next();
 };
 
